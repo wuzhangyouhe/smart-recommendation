6ca6026390c49d9db9b2968a42de413dd3115673
LT <liutao272@gmail.com>
2018-07-23 15:51:10 +0800

    only using SVD algorithm

--- 3-skicit-learning-svd.py
+++ 3-skicit-learning-svd.py
@@ -37,20 +37,17 @@ print x.getUsers().shape, '\n' , x.getRatings().shape, '\n' , x.getDeals().shape
 print x.getUsers().head(10) , '\n', x.getRatings().head(), '\n', x.getDeals().head()
 print x.getUsers().describe(), '\n', x.getRatings().describe(), '\n', x.getDeals().describe()
 
-# x.getUsers().hist()
+# x.getUsers().hist(figsize=(6,6))
 # plt.show()
 # x.getRatings().hist()
 # plt.show()
-# x.getDeals().hist(figsize=(16,16))
+# x.getDeals().hist(figsize=(12,8))
 # plt.show()
 
 n_users = x.getRatings().user_id.unique().shape[0]
 n_items = x.getRatings().food_id.unique().shape[0]
 
 train_data, test_data = cv.train_test_split(x.getRatings(), test_size = 0.30)
-
-# Memory-based collaborative filtering
-print "1, Memory-based collaborative filtering \n"
 train_data_matrix = np.zeros((n_users, n_items))
 for line in train_data.itertuples():
     train_data_matrix[line[1]-1, line[2]-1] = line[3]
@@ -59,30 +56,33 @@ test_data_matrix = np.zeros((n_users, n_items))
 for line in test_data.itertuples():
     test_data_matrix[line[1]-1, line[2]-1] = line[3]
 
-from sklearn.metrics.pairwise import pairwise_distances
-user_similarity = pairwise_distances(train_data_matrix, metric='cosine')
-item_similarity = pairwise_distances(train_data_matrix.T, metric='cosine')
-
-def predict(ratings, similarity, type='user'):
-    if type == 'user':
-        mean_user_rating = ratings.mean(axis=1)
-        ratings_diff = (ratings - mean_user_rating[:, np.newaxis])
-        pred = mean_user_rating[:, np.newaxis] + similarity.dot(ratings_diff) / np.array([np.abs(similarity).sum(axis=1)]).T
-    elif type == 'item':
-        pred = ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
-    return pred
-
-item_prediction = predict(train_data_matrix, item_similarity, type='item')
-user_prediction = predict(train_data_matrix, user_similarity, type='user')
-
 def rmse(prediction, ground_truth):
     prediction = prediction[ground_truth.nonzero()].flatten()
     ground_truth = ground_truth[ground_truth.nonzero()].flatten()
     return sqrt(mean_squared_error(prediction, ground_truth))
 
-print 'User-based CF RMSE: ' + str(rmse(user_prediction, test_data_matrix))
-print 'Item-based CF RMSE: ' + str(rmse(item_prediction, test_data_matrix))
-print user_prediction[0], '\n', item_prediction[0]
+# Memory-based collaborative filtering
+# print "1, Memory-based collaborative filtering \n"
+# from sklearn.metrics.pairwise import pairwise_distances
+# user_similarity = pairwise_distances(train_data_matrix, metric='cosine')
+# item_similarity = pairwise_distances(train_data_matrix.T, metric='cosine')
+#
+# def predict(ratings, similarity, type='user'):
+#     if type == 'user':
+#         mean_user_rating = ratings.mean(axis=1)
+#         ratings_diff = (ratings - mean_user_rating[:, np.newaxis])
+#         pred = mean_user_rating[:, np.newaxis] + similarity.dot(ratings_diff) / np.array([np.abs(similarity).sum(axis=1)]).T
+#     elif type == 'item':
+#         pred = ratings.dot(similarity) / np.array([np.abs(similarity).sum(axis=1)])
+#     return pred
+#
+# item_prediction = predict(train_data_matrix, item_similarity, type='item')
+# user_prediction = predict(train_data_matrix, user_similarity, type='user')
+#
+#
+# print 'User-based CF RMSE: ' + str(rmse(user_prediction, test_data_matrix))
+# print 'Item-based CF RMSE: ' + str(rmse(item_prediction, test_data_matrix))
+# print user_prediction[0], '\n', item_prediction[0]
 
 # Model-based collaborative filter
 print "2, Model-based collaborative filter \n"
