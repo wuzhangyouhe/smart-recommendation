80a84fae7d2616280965692a84526a0d2d57a068
Tao Liu (Singapore) <liutao@juniper.net>
2018-08-28 20:43:55 +0800

    accuracy validation

--- 2-skicit-learning-svd.py
+++ 2-skicit-learning-svd.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import cross_validation as cv
 from scipy.sparse.linalg import svds
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 import json
+
 
 class readDataset :
 
@@ -29,6 +34,57 @@ class readDataset :
         deals = pd.read_csv(self.dp, sep=',', names = d_cols, encoding='latin-1')
         return deals
 
+x = readDataset('ml-100k/u.user', 'ml-100k/u.data', 'ml-100k/u.item1')
+print x.getUsers().shape, '\n' , x.getRatings().shape, '\n' , x.getDeals().shape
+print x.getUsers().head(10) , '\n', x.getRatings().head(), '\n', x.getDeals().head()
+print x.getUsers().describe(), '\n', x.getRatings().describe(), '\n', x.getDeals().describe()
+
+x.getUsers().hist(figsize=(6,6))
+plt.show()
+x.getRatings().hist()
+plt.show()
+x.getDeals().hist(figsize=(12,8))
+plt.show()
+
+users = x.getRatings().user_id.unique().shape[0]
+deals = x.getRatings().deal_id.unique().shape[0]
+
+I_train_data, F_test_data = cv.train_test_split(x.getRatings(), test_size = 0.20)
+train_data, test_data = cv.train_test_split(I_train_data, test_size = 0.20)
+
+train_data_matrix = np.zeros((users, deals))
+for line in train_data.itertuples():
+    train_data_matrix[line[1]-1, line[2]-1] = line[3]
+print train_data_matrix, line
+
+test_data_matrix = np.zeros((users, deals))
+for line in test_data.itertuples():
+    test_data_matrix[line[1]-1, line[2]-1] = line[3]
+
+F_test_data_matrix = np.zeros((users, deals))
+for line in F_test_data.itertuples():
+    F_test_data_matrix[line[1]-1, line[2]-1] = line[3]
+    
+def rmse(prediction, ground_truth):
+    prediction = prediction[ground_truth.nonzero()].flatten()
+    ground_truth = ground_truth[ground_truth.nonzero()].flatten()
+    return sqrt(mean_squared_error(prediction, ground_truth))
+
+# Model-based collaborative filter
+sparsity=round(1.0-len(x.getRatings())/float(users*deals),3)
+print 'The sparsity level of Deal amount 100k is ' +  str(sparsity*100) + '%'
+
+trigger_event= json.dumps({ "deal id": 123 }, indent=4, separators=(',', ': '))
+
+# SVD(Singular value decomposition) algorithm
+# get SVD components from train matrix. Choose k.
+u, s, vt = svds(train_data_matrix, k = 50)
+s_diag_matrix=np.diag(s)
+X_pred = np.dot(np.dot(u, s_diag_matrix), vt)
+print 'User-based CF RMSE: ' + str(rmse(X_pred, test_data_matrix))
+
+print 'Final User-based CF RMSE: ' + str(rmse(X_pred, F_test_data_matrix))
+
 def top_cosine_similarity(data, deal_id, top_n):
     index = deal_id - 1 # Deal id starts from 1
     deal_row = data[index, :]
@@ -38,35 +94,15 @@ def top_cosine_similarity(data, deal_id, top_n):
     return sort_indexes[:top_n]
 
 # Helper function to print top N similar movies
-def print_similar_deals(deal_data, deal_id, top_indexes):
+def print_similar_movies(deal_data, deal_id, top_indexes):
     print('\n Recommendations for {0}: \n'.format(deal_data[deal_data.deal_id == deal_id].deal_title.values[0]))
-    rcmmd = []
-    for id in top_indexes[1:]:
-        child_rcmmd = {}
-        child_rcmmd['Deal id'] = id
-        child_rcmmd['Deal name'] = deal_data[deal_data.deal_id == id].deal_title.values[0]
-        rcmmd.append(child_rcmmd)
-    print json.dumps(rcmmd, indent=4, separators=(',',':'))
+    for id in top_indexes + 1:
+        print(id, deal_data[deal_data.deal_id == id].deal_title.values[0])
+        
+k = 50
+deal_id = 10 # Grab an id from items table
+top_n = 10
 
-
-trigger_event= json.dumps({ "deal id": 123 }, indent=4, separators=(',', ': '))
-
-
-def main():
-    x = readDataset('ml-100k/u.user', 'ml-100k/u.data', 'ml-100k/u.item1')
-    users = x.getRatings().user_id.unique().shape[0]
-    deals = x.getRatings().deal_id.unique().shape[0]
-    current_data = np.zeros((users, deals))
-    # SVD(Singular value decomposition) algorithm
-    u, s, vt = svds(current_data, k = 50)
-    event = json.loads(trigger_event)
-    k = 50
-    deal_id = event["deal id"] # Grab an id from items table
-    top_n = 11
-
-    sliced = vt.T[:, :k] # representative data
-    indexes = top_cosine_similarity(sliced, deal_id, top_n)
-    result = print_similar_deals(x.getDeals(), deal_id, indexes)
-    return result
-
-main()
+sliced = vt.T[:, :k] # representative data
+indexes = top_cosine_similarity(sliced, deal_id, top_n)
+print_similar_movies(x.getDeals(), deal_id, indexes)
--- 3-skicit-learning-svd-bk.py
+++ 3-skicit-learning-svd-bk.py
@@ -5,6 +5,7 @@ from sklearn import cross_validation as cv
 from scipy.sparse.linalg import svds
 from sklearn.metrics import mean_squared_error
 from math import sqrt
+import json
 
 class readDataset :
 
@@ -19,15 +20,15 @@ class readDataset :
         return users
 
     def getRatings (self):
-        r_cols = ['user_id', 'food_id', 'rating', 'unix_timestamp']
+        r_cols = ['user_id', 'deal_id', 'rating', 'unix_timestamp']
         ratings = pd.read_csv(self.rp, sep='\t', names = r_cols, encoding='latin-1')
         return ratings
 
     def getDeals (self):
-        d_cols = ['food_id', 'food_title', 'publish date', 'food release date', 'IMDb URL', 'yeast', ' salt',
-                  ' coconut oil',
-                  'Deluxe Nut Mix	', ' sugar', 'honey', 'pecans', 'wheat', ' organic oat flour', 'onion',
-                  ' garlic', 'malt', 'barley', 'soybeans', ' citric acid', 'promo', 'discount 25%', 'discount 50%',
+        d_cols = ['deal_id', 'deal_title', 'publish date', 'create date', 'IMDb URL', 'SG Fathers Day', ' SG Ramadan',
+                  ' SG New Year day',
+                  'SG Christmas', ' SG July 14th', 'SG Mothers Day', 'SG Super Sunday', 'SG Fantastic Friday', 'HK Fathers Day', 'HK Ramadan',
+                  ' HK New Years day', 'HK Christmas', 'HK July 14th', 'HK Mothers Day', ' HK Super Sunday', 'HK Fantastic Friday', 'discount 25%', 'discount 50%',
                   'discount 75%']
         deals = pd.read_csv(self.dp, sep=',', names = d_cols, encoding='latin-1')
         return deals
@@ -37,12 +38,12 @@ print x.getUsers().shape, '\n' , x.getRatings().shape, '\n' , x.getDeals().shape
 print x.getUsers().head(10) , '\n', x.getRatings().head(), '\n', x.getDeals().head()
 print x.getUsers().describe(), '\n', x.getRatings().describe(), '\n', x.getDeals().describe()
 
-# x.getUsers().hist(figsize=(6,6))
-# plt.show()
-# x.getRatings().hist()
-# plt.show()
-# x.getDeals().hist(figsize=(12,8))
-# plt.show()
+x.getUsers().hist(figsize=(6,6))
+plt.show()
+x.getRatings().hist()
+plt.show()
+x.getDeals().hist(figsize=(12,8))
+plt.show()
 
 # class train_function :
 #
@@ -52,7 +53,7 @@ print x.getUsers().describe(), '\n', x.getRatings().describe(), '\n', x.getDeals
 #     def split_dataset(self):
 #
 #         users = self.ratingData.user_id.unique().shape[0]
-#         deals = self.ratingData.food_id.unique().shape[0]
+#         deals = self.ratingData.deal_id.unique().shape[0]
 #
 #         train_data, test_data = cv.train_test_split(x.getRatings(), test_size=0.30)
 #         train_data_matrix = np.zeros((users, deals))
@@ -73,7 +74,7 @@ print x.getUsers().describe(), '\n', x.getRatings().describe(), '\n', x.getDeals
 # n_users = train_function(x.getRatings()).split_dataset()
 
 users = x.getRatings().user_id.unique().shape[0]
-deals = x.getRatings().food_id.unique().shape[0]
+deals = x.getRatings().deal_id.unique().shape[0]
 
 train_data, test_data = cv.train_test_split(x.getRatings(), test_size = 0.30)
 train_data_matrix = np.zeros((users, deals))
@@ -123,8 +124,8 @@ s_diag_matrix=np.diag(s)
 X_pred = np.dot(np.dot(u, s_diag_matrix), vt)
 print 'User-based CF RMSE: ' + str(rmse(X_pred, test_data_matrix))
 
-def top_cosine_similarity(data, food_id, top_n=10):
-    index = food_id - 1 # Food id starts from 1
+def top_cosine_similarity(data, deal_id, top_n=10):
+    index = deal_id - 1 # Food id starts from 1
     food_row = data[index, :]
     magnitude = np.sqrt(np.einsum('ij, ij -> i', data, data))
     similarity = np.dot(food_row, data.T) / (magnitude[index] * magnitude)
@@ -132,18 +133,23 @@ def top_cosine_similarity(data, food_id, top_n=10):
     return sort_indexes[:top_n]
 
 # Helper function to print top N similar movies
-def print_similar_movies(food_data, food_id, top_indexes):
-    print('\n Recommendations for {0}: \n'.format(food_data[food_data.food_id == food_id].food_title.values[0]))
-    for id in top_indexes + 1:
-        print(food_data[food_data.food_id == id].food_title.values[0])
+def print_similar_movies(food_data, deal_id, top_indexes):
+    print('\n Recommendations for {0}: \n'.format(food_data[food_data.deal_id == deal_id].deal_title.values[0]))
+    rcmmd = []
+    for id in top_indexes[1:]:
+        child_rcmmd = {}
+        child_rcmmd['Deal id'] = id
+        child_rcmmd['Deal name'] = food_data[food_data.deal_id == id].deal_title.values[0]
+        rcmmd.append(child_rcmmd)
+    print json.dumps(rcmmd, indent=4, separators=(',',':'))
         
 k = 50
-food_id = 110 # Grab an id from items table
+deal_id = 110 # Grab an id from items table
 top_n = 10
 
 sliced = vt.T[:, :k] # representative data
-indexes = top_cosine_similarity(sliced, food_id, top_n)
-print_similar_movies(x.getDeals(), food_id, indexes)
+indexes = top_cosine_similarity(sliced, deal_id, top_n)
+print_similar_movies(x.getDeals(), deal_id, indexes)
 
 # # SVD and PCA relationship
 # normalised_mat = ratings_mat - np.matrix(np.mean(ratings_mat, 1)).T
