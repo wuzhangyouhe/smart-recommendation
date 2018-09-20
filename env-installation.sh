2975f829e123b3f36c873b58530a20638fe3ffca
LT <liutao272@gmail.com>
2018-09-20 01:00:10 +0000

    Update env-installation.sh

--- env-installation.sh
+++ env-installation.sh
@@ -1,26 +1,28 @@
 #!/bin/bash
 
+cd chooosie-deals-intelligent-system
 #virtualenv lambda_prototype
 source lambda_prototype/bin/activate
-apt-get install -y build-essential libssl-dev libffi-dev python-dev python-tk tcpflow git curl vim
-curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
-python get-pip.py
-pip install numpy pandas SciPy
+#apt-get install -y build-essential libssl-dev libffi-dev python-dev python-tk tcpflow git curl vim
+#curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
+#python get-pip.py
+#pip install numpy pandas SciPy
  
-pip install scikit-learn 
-pip install matplotlib
+#pip install scikit-learn 
+#pip install matplotlib
  
-pip install csvkit 
-pip install pydotplus 
-pip install graphviz
+#pip install csvkit 
+#pip install pydotplus 
+#pip install graphviz
  
-pip install Ipython
-pip install numpy pandas SciPy
-pip install scikit-learn 
-pip install matplotlib
-pip install csvkit 
-pip install pydotplus 
-pip install graphviz
-pip install Ipython
-pip install tensorrec
-pip install flask flask-jsonpify flask-sqlalchemy flask-restful
+#pip install Ipython
+#pip install numpy pandas SciPy
+#pip install scikit-learn 
+#pip install matplotlib
+#pip install csvkit 
+#pip install pydotplus 
+#pip install graphviz
+#pip install Ipython
+#pip install tensorrec
+#pip install flask flask-jsonpify flask-sqlalchemy flask-restful
+python ms-mlengine-local.py
