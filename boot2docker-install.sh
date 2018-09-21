08924d624df86e189093395a239a6e64f0755ce4
LT <liutao272@gmail.com>
2018-09-21 21:58:32 +0000

    Add new file

---
+++ boot2docker-install.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+tce-load -wi python
+curl https://bootstrap.pypa.io/get-pip.py | sudo python -
+pip install numpy pandas SciPy
+ 
+pip install scikit-learn 
+pip install matplotlib
+ 
+pip install csvkit 
+pip install pydotplus 
+pip install graphviz
+ 
+pip install Ipython
+pip install numpy pandas SciPy
+pip install scikit-learn 
+pip install matplotlib
+pip install csvkit 
+pip install pydotplus 
+pip install graphviz
+pip install Ipython
+pip install tensorrec
+pip install flask flask-jsonpify flask-sqlalchemy flask-restful
+python ms-mlengine-local.py
