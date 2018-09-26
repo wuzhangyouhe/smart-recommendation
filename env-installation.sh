c24cc6eddfa9e7f119819e3bc22c802a00c3896e
LT <liutao272@gmail.com>
2018-09-26 01:44:29 +0000

    Update env-installation.sh

--- env-installation.sh
+++ env-installation.sh
@@ -2,7 +2,7 @@
 
 #virtualenv lambda_prototype
 source lambda_prototype/bin/activate
-apt-get install -y build-essential libssl-dev libffi-dev python-dev python-tk tcpflow curl
+apt-get install -y build-essential libssl-dev libffi-dev python-dev python-tk curl
 curl "https://bootstrap.pypa.io/get-pip.py" -o "get-pip.py"
 python get-pip.py
 pip install numpy pandas SciPy
